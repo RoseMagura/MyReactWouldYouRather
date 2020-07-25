e8f9f38559996471a2a8c9e21e8d9738a9ebb11c
Rosie Magura <magurarm@guilford.edu>
2020-07-25 19:33:16 -0400

    Feat: Add set up for leaderboard

--- would-you-rather/src/components/Leaderboard.js
+++ would-you-rather/src/components/Leaderboard.js
@@ -3,18 +3,48 @@ import { connect } from 'react-redux';
 import Nav from './Nav';
 
 class Leaderboard extends Component {
-
     render(){
+        // console.log(this.props)
+        const { usersArray, authedUser, users } = this.props
+        // console.log('USERS: ', usersArray)
+
+        let answersEach = {}
+        users['sarahedo'] !== undefined &&
+            usersArray.forEach(user => {
+                answersEach[user] = Object.keys(users[user].answers).length
+            })
+
+        let questionsEach = {}
+        users['sarahedo'] !== undefined &&
+            usersArray.forEach(user => {
+                questionsEach[user] = Object.keys(users[user].questions).length
+                })    
+        
+        usersArray.forEach(element => console.log(element, 
+            answersEach[element], questionsEach[element]))        
+        // console.log('Authed User: ', authedUser)
         return(
             <div>
                 <Nav />
-                LEADERBOARD
+                <ul className='users-list'>
+                    {usersArray.map((u) => (
+                        <li key={u}>
+                            {u}
+                            {Object.keys(users[`${u}`].answers).length} answers
+                            {Object.keys(users[`${u}`].questions).length} questions 
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }}
-function mapStateToProps ({ authedUser }) {
+function mapStateToProps ({ authedUser, users }) {
         return {
-          authedUser
+          authedUser, 
+          users,
+          //want userid, number of answers, number of questions
+          usersArray: Object.keys(users)
+            // .sort((a, b) => users[a].answers.length - users[b].answers.length)
         }
       }
 export default connect(mapStateToProps)(Leaderboard)
